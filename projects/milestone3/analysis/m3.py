bef37008c2ac15c0dda68d7e3adc5d36b9947b71
Vetle Vikenes <vetle.vkns@gmail.com>
2023-04-12 09:08:12 +0200

    minor stuff

---
+++ projects/milestone3/analysis/m3.py
@@ -0,0 +1,286 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+import astropy.units as u 
+import os 
+import warnings 
+warnings.filterwarnings("ignore", category=DeprecationWarning )
+
+from scipy.integrate import simpson 
+
+
+"""
+All calculations and such are performed in this file,
+but the actual plotting is done in plot.py
+"""
+import plot
+
+data_path = "/home/vetle/Documents/master_studies/subjects/V23/AST5220/projects/milestone3/data/"
+
+global SAVE 
+global TEMP
+global RECOMBINATION
+SAVE        = False 
+TEMP        = False 
+RECOMBINATION  = False 
+
+
+class Recombination:
+    def __init__(self, 
+                 filename,
+                 rec_and_dec_times_fname=None, 
+                 length_unit=u.Mpc,
+                 time_unit=u.yr):
+        
+        self.data           = self.load_data(filename)
+        self.x              = self.data[0]
+
+        self.rec_dec_file   = rec_and_dec_times_fname
+        self.rec_dec_saha   = rec_and_dec_times_fname.strip(".txt") + "_saha.txt"
+
+        self.length_unit    = length_unit
+        self.time_unit      = time_unit
+
+
+
+    
+    def load_data(self, filename, skiprows=2):
+        return np.loadtxt(data_path + filename, unpack=True, skiprows=skiprows)
+    
+    def load_x_decoupling(self):
+        x = np.loadtxt(data_path + self.rec_dec_file, skiprows=1, usecols=(1,2))[0]
+        self.xLSS     = x[0]
+        self.xrec     = x[1]
+
+    
+    def load_Xe(self):
+        self.Xe             = self.data[1]
+        
+    def load_ne(self, convert_unit=True):
+        self.ne     = self.data[2] * u.m**(-3) 
+        if convert_unit:
+            self.ne = self.ne.to(self.length_unit**(-3))
+
+    def load_taus(self):
+        self.tau            = self.data[3]
+        self.dtau_dx        = self.data[4]
+        self.ddtau_ddx      = self.data[5]
+
+    def load_g(self):
+        self.g_tilde        = self.data[6]
+        self.dg_tilde_dx    = self.data[7]
+        self.ddg_tilde_ddx  = self.data[8]
+
+    def load_sound_horizon(self, convert_unit=True):
+        self.sound_horizon = self.data[9] * u.m 
+        if convert_unit:
+            self.sound_horizon = self.sound_horizon.to(self.length_unit)
+
+    def x_to_redshift(self, x):
+        # Convert from x to redshift 
+        return np.exp(-x) - 1 
+    
+    ##########################################
+    # Make use of the times data in the class 
+    ##########################################
+    def assert_valid_recombination_value(self, print_x=False, stop=False):
+        """
+        Check that recombination occurs at z in [1050, 1150]
+        both via tau(x_rec)=1 and g_tilde(x_rec)=max(g_tilde)
+        """
+        if hasattr(self, 'tau'):
+            pass
+        else:
+            self.load_taus()
+
+        if hasattr(self, 'g_tilde'):
+            pass
+        else:
+            self.load_g()
+
+        tau_equal_1_idx = np.argmin(np.abs(self.tau - 1))
+        g_peak_idx      = np.argmax(self.g_tilde)
+        x_rec_tau = self.x[tau_equal_1_idx]
+        x_rec_g= self.x[g_peak_idx]
+        z_rec_tau = self.x_to_redshift(x_rec_tau)
+        z_rec_g   = self.x_to_redshift(x_rec_g)
+    
+
+        if z_rec_tau < 1050 or z_rec_tau > 1150:
+            print(" ")
+            print("### ERROR ###")
+            print("Invlaid solution. tau=1 at z outside [1050, 1150]")
+            print_x = True 
+            stop = True  
+
+        elif z_rec_g < 1050 or z_rec_g > 1150:
+            print("### ERROR ###")
+            print("Invlaid solution. g peaks at z outside [1050, 1150]")
+            print_x = True 
+            stop = True 
+
+        if print_x:
+            print(f"z(tau=1) = {z_rec_tau:.5f}, x={x_rec_tau:.5f}")
+            print(f"z(g_max) = {z_rec_g:.5f}, x={x_rec_g:.5f}")
+
+        if stop:
+            exit()
+
+    def assert_normalized_g_tilde(self, tol=1e-4, print_g=False, stop=False):
+        """
+        Check that g_tilde is sufficiently normalized to 1
+        by checking whether abs(g-1) > tol   
+        """
+
+        if hasattr(self, 'g_tilde'):
+            pass
+        else:
+            self.load_g()
+
+        g_integrated = simpson(self.g_tilde, self.x)
+        delta_g_norm = abs(g_integrated - 1)
+        if delta_g_norm > tol:
+            print("### WARNING ###")
+            print("g_tilde(x) not properly normalized.")
+            print_g = True 
+            stop = True
+        
+        if print_g:
+            print(f"  int g(x) deviation = {delta_g_norm:.5e}")
+
+        if stop:
+            exit() 
+
+    def Xe_today(self):
+        self.load_Xe()
+        Xe0 = self.Xe[-1]
+        print(self.x[-1])
+        print(f"X_e at x={self.x[-1]:.2f} is: {self.Xe[-1]:.5e}")
+
+    def compare_Xe(self, x_saha, Xe_saha, 
+                   xrec_saha=None,
+                   ylim=[1e-4, 2], xlim=[-7.8,-5.3],
+                   figname="compare_Xe_peebles_saha.pdf"):
+        
+        if hasattr(self, 'Xe'):
+            pass
+        else:
+            self.load_Xe()
+
+        if RECOMBINATION:
+            if not hasattr(self, "xrec"):
+                self.load_x_decoupling()
+            
+            xrec_peebles = self.xrec
+
+
+        plot.compare_Xe_peebles_and_saha(self.x, x_saha,
+                                         self.Xe, Xe_saha,
+                                         xrec_peebles=xrec_peebles, xrec_saha=xrec_saha,
+                                         fname=figname,
+                                         xlim=xlim, ylim=ylim,
+                                         rec_times=RECOMBINATION,
+                                         save=SAVE, temp=TEMP)
+
+
+
+    def plot_visibility_functions(self, dg_dx_scaling=10, 
+                                        ddg_ddx_scaling=300,
+                                        xlim=[-7.5, -6],
+                                        ylim=None,
+                                        figname="g_plot.pdf"):
+        
+        """
+        Plot g(x), g'(x) and g''(x)
+        scale g'(x) and g''(x) to fit in the same plot as g(x)
+        """
+
+        if hasattr(self, 'g_tilde'):
+            pass
+        else:
+            self.load_g()
+
+        dg_dx_scaled = self.dg_tilde_dx / dg_dx_scaling
+        ddg_ddx_scaled = self.ddg_tilde_ddx / ddg_ddx_scaling
+
+        x   = self.x 
+        y   = self.g_tilde
+        dy  = dg_dx_scaled 
+        ddy = ddg_ddx_scaled
+       
+
+        y_legend    = r"$\tilde{g}(x)$"
+        dy_legend   = r"$\tilde{g}'(x)$" + rf"$/{str(dg_dx_scaling)}$"
+        ddy_legend  = r"$\tilde{g}''(x)$" + rf"$/{str(ddg_ddx_scaling)}$"
+
+
+        plot.plot_quantity_with_derivatives(x, y, dy, ddy, 
+                                            y_legend, dy_legend, ddy_legend,
+                                            fname=figname,
+                                            xlim=xlim, log=False,
+                                            save=SAVE, temp=TEMP)
+
+
+    def plot_tau_with_derivatives(self, xlim=[-10,0], ylim=[1e-8, 1e6],
+                                  figname="tau_plot.pdf"):
+
+        if hasattr(self, 'tau'):
+            pass
+        else:
+            self.load_taus()
+
+        x   = self.x 
+        y   = self.tau 
+        dy  = - self.dtau_dx
+        ddy = self.ddtau_ddx
+
+        y_legend   = r"$\tau(x)$"
+        dy_legend  = r"$- \tau'(x)$"
+        ddy_legend = r"$\tau''(x)$"
+
+        plot.plot_quantity_with_derivatives(x, y, dy, ddy, 
+                                            y_legend, dy_legend, ddy_legend,
+                                            fname="tau_plot.pdf",
+                                            xlim=xlim, ylim=ylim,
+                                            save=SAVE, temp=TEMP)
+
+
+    def make_table(self, saha=False):
+        if saha:
+            fname = self.rec_dec_saha
+        else:
+            fname = self.rec_dec_file
+
+        x, z, t, r = np.loadtxt(data_path + fname, skiprows=1, usecols=(1,2)) 
+
+        
+        t       = (t * u.s).to(self.time_unit).value 
+        r       = (r * u.m).to(self.length_unit).value
+
+
+        plot.time_table(x, z, t, r,
+                        saha=saha,
+                        save=SAVE,
+                        temp=TEMP)
+
+
+
+
+
+
+rec = Recombination(filename="recombination.txt", 
+                    rec_and_dec_times_fname="rec_times.txt"
+                    )
+
+rec_saha_only = Recombination(filename="recombination_saha.txt", 
+                              rec_and_dec_times_fname="rec_times_saha.txt"
+                              )
+rec_saha_only.load_Xe()
+x_saha, Xe_saha = rec_saha_only.x, rec_saha_only.Xe
+
+rec_saha_only.load_x_decoupling()
+
+x_rec_saha = rec_saha_only.xrec
+
+rec.assert_valid_recombination_value()
+rec.assert_normalized_g_tilde()
+
---
+++ projects/milestone3/analysis/plot.py
@@ -0,0 +1,280 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+import astropy.units as u 
+from astropy.constants import c  
+import os 
+from matplotlib.ticker import ScalarFormatter, FormatStrFormatter
+import pandas as pd 
+
+import warnings 
+warnings.filterwarnings("ignore", category=DeprecationWarning )
+warnings.filterwarnings("ignore", category=FutureWarning )
+
+
+
+
+#   rc and plot params
+TICKLABELSIZE = 20
+LABELSIZE     = 25
+LEGENDSIZE    = 20
+TITLESIZE     = 30
+LABELPAD      = 1
+TITLEPAD      = 15
+
+
+#   Set rc-params
+plt.rc("legend", fontsize=LEGENDSIZE, fancybox=True, loc="best", frameon=True, edgecolor="black")
+plt.rc("font", size=25)
+plt.rc("axes", titlesize=TITLESIZE, labelsize=LABELSIZE, labelpad=LABELPAD, titlepad=TITLEPAD)
+plt.rc("xtick", labelsize=TICKLABELSIZE)
+plt.rc("ytick", labelsize=TICKLABELSIZE)
+# plt.rc("ytick.minor", pad=1)
+plt.rc("lines", linewidth=2.5)
+plt.rc('text', usetex=True)
+# plt.rc("label", fontsize=20)
+
+plt.rcParams['savefig.bbox'] = 'tight'
+plt.rcParams['font.family'] = 'Times New Roman'
+
+
+
+# Paths 
+# project_path = os.path.abspath(".")
+project_path = "/home/vetle/Documents/master_studies/subjects/V23/AST5220/projects/milestone3"
+
+data_path = project_path + "/data/"
+latex_path = project_path + "/report/tables/"
+pdf_path = project_path + "/analysis/figures/"
+png_path = pdf_path + "temp/" 
+
+
+def save_push(fig, pdf_name, save=True, push=False, show=False, tight=True, temp=False):
+    """
+    This function handles whether you want to show,
+    save and/or push the file to git.
+    Args:
+        fig (matplotlib.figure): Figure you want to handle
+        pdfname (string): Name of output pdf file
+        args (argparse)
+    """
+    if tight:
+        fig.tight_layout()
+
+    if temp:
+        fig_name = pdf_name.replace(".pdf", ".png")
+        fig_path = png_path
+    else: 
+        fig_name = pdf_name
+        fig_path = pdf_path 
+    
+    file = fig_path + fig_name
+
+
+    if save:
+        print(f'Saving plot: {fig_name}')
+        if not temp:
+            fig.savefig(file)
+        else:
+            fig.savefig(file, dpi=50)
+    
+    else:
+        plt.show()
+
+    if push:
+        os.system(f"git add {file}")
+        os.system("git commit -m 'upload plot'")
+        os.system("git push")
+
+    plt.close()
+
+
+def set_ax_info(ax, xlabel, ylabel=False, title=None, legend=True, legendloc='best', legend_size=False):
+    """Write title and labels on an axis with the correct fontsizes.
+    Args:
+        ax (matplotlib.axis): the axis on which to display information
+        title (str): the desired title on the axis
+        xlabel (str): the desired lab on the x-axis
+        ylabel (str): the desired lab on the y-axis
+    """
+    ax.set_xlabel(xlabel)
+    if ylabel != False:
+        ax.set_ylabel(ylabel)
+    ax.set_title(title)
+    
+    # ax.tick_params(axis='both', which='major', labelsize=15)
+    # ax.yaxis.get_offset_text().set_fontsize(15)
+    # try:
+        # ax.ticklabel_format(style=style)
+    # except AttributeError:
+        # pass
+    if legend:
+        if legend_size:
+            ax.legend(loc=legendloc, fontsize=legend_size)
+        else:
+
+            ax.legend(loc=legendloc)
+
+
+
+
+# -----------------------------------------------------------------------------
+#   General plot code above
+#   XXX
+#   Specific plot code below
+# -----------------------------------------------------------------------------
+
+
+
+def load(file, folder=data_path, skiprows=0):
+    # Tired of repeating unpack, delimiter, skiprows for all... 
+    return np.loadtxt(folder + file, unpack=True, skiprows=skiprows)
+
+
+
+def plot_quantity_with_derivatives(x, y, dy, ddy,
+                                   y_legend, dy_legend, ddy_legend, 
+                                   fname, xlabel=r"$x$", ylabel=None, 
+                                   xlim=None, ylim=None, 
+                                   legendloc='best', yticks=None, 
+                                   figsize=(8,6), log=True, 
+                                   save=True, temp=False):
+
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.plot(x, y  ,ls='solid' , color='blue'  , label=y_legend)
+    ax.plot(x, dy ,ls='dashed', color='orange', label=dy_legend)
+    ax.plot(x, ddy,ls='dotted', color='green' , label=ddy_legend)
+
+    
+    ax.set_ylim(ylim)
+    ax.set_xlim(xlim)
+
+    if log:
+        ax.set_yscale('log')
+    
+    set_ax_info(ax, xlabel, ylabel, legendloc=legendloc)
+
+    if yticks is not None:
+        ax.set_yticks(yticks)
+
+    save_push(fig, fname, save, temp=temp)
+    
+
+
+def compare_Xe_peebles_and_saha(x_peebles, x_saha, 
+                                Xe_peebles, Xe_saha,
+                                xrec_peebles, xrec_saha,
+                                fname, 
+                                rec_times=False,
+                                   xlim=None, ylim=None, 
+                                   legendloc='best', yticks=None, 
+                                   figsize=(8,6), log=True, 
+                                   save=True, temp=False):
+
+
+    fig, ax = plt.subplots(figsize=figsize)
+
+    ax.plot(x_peebles, Xe_peebles ,ls='solid' , color='blue'  , label="Peebles")
+    ax.plot(x_saha   , Xe_saha    ,ls='dashed', color='green', label="Saha")
+
+    if rec_times:
+        ax.vlines(xrec_peebles, *ylim, ls='dotted', color='black'  , alpha=1, label="Recombination, Peebles")
+        ax.vlines(xrec_saha   , *ylim, ls='dotted', color='red', alpha=1, label="Recombination, Saha")
+
+        fname = "recombination_" + fname 
+
+    
+    ax.set_ylim(ylim)
+    ax.set_xlim(xlim)
+
+    if log:
+        ax.set_yscale('log')
+
+    
+    xlabel = r"$x$"
+    ylabel = r"$X_e$"
+
+    set_ax_info(ax, xlabel, ylabel, legendloc=legendloc)
+
+    if yticks is not None:
+        ax.set_yticks(yticks)
+
+    save_push(fig, fname, save, temp=temp)
+
+
+
+def time_table(x, z, t, r, saha=False, save=False, temp=False):
+   
+
+    if saha:
+        method = "Saha"
+    else:
+        method = "Peebles"
+
+    col_labels = [method, r"$x$", r"$z$", r"$t\,\mathrm{[yr]}$"]
+    col_fmt = "l|ccc"
+
+    if not saha:
+        col_labels.append(r"$r_s \,\mathrm{[Mpc]}$")
+        col_fmt += "c"
+
+    row_labels = [r"Decoupling",
+                  r"Recombination"]
+    
+    t1_ = str(np.round(t[0]))
+    t2_ = str(np.round(t[1]))
+
+    t1 = t1_[0:3] + "\," + t1_[3:6]
+    t2 = t2_[0:3] + "\," + t2_[3:6]
+
+    t = [t1, t2]
+
+    # x = np.round(x,3)
+    z = np.round(z,1)
+
+    if saha:
+        data = np.array([row_labels, x, z, t]).T 
+    else:
+        r = [np.round(r[0],2), " "]
+        
+
+        data = np.array([row_labels, x, z, t, r]).T 
+
+
+    table_caption   = "Times when decoupling and recombination occurs, computed from the " 
+    table_caption   += method + " equation."
+
+    table_name      = "rec_and_dec_time_table_" + method
+    table_label     = "tab:M2:results:" + table_name
+    table_fname     = table_name + ".tex"
+
+    if save:
+        if temp:
+            table_fname = "TEMP" + table_fname
+
+        buffer = latex_path + table_fname 
+    else:
+        buffer = None 
+
+
+    table = pd.DataFrame(data).to_latex(
+            index=False, 
+            header=col_labels, 
+            escape=False, 
+            column_format=col_fmt,
+            caption=table_caption,
+            label=table_label,
+            position="h",
+            buf=buffer
+        )
+
+
+    if save:
+        print(f"Saving time table for {method}:")
+        print(f"   {table_fname}")
+        
+    else:
+        print(table)
+
+
+if __name__=='__main__':
+    pass 
